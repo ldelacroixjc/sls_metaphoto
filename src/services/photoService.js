ac129118eecc967cc982a0c5fc786c96423125bb
Juan Carlos de la Cruz <ldelacroixjc@gmail.com>
2024-07-08 01:06:12 -0600

    mejorar legibilidad del codigo

--- src/services/photoService.js
+++ src/services/photoService.js
@@ -7,13 +7,11 @@ const getPhotos = async (params) => {
         const PHOTOS_URL = `${process.env.API_URL}/photos`;
         const ALBUMS_URL =  `${process.env.API_URL}/albums`;
         const USERS_URL =  `${process.env.API_URL}/users`;
-
         const [photosResponse, albumsResponse, usersResponse] = await Promise.all([
             request.getData(PHOTOS_URL),
             request.getData(ALBUMS_URL),
             request.getData(USERS_URL)
         ])
-
         if( 
             photosResponse.status === 200 && 
             albumsResponse.status === 200 && 
@@ -22,41 +20,56 @@ const getPhotos = async (params) => {
             const photos = photosResponse.data;
             const users = utils.convertToMap(usersResponse.data);
             const albums = utils.convertToMap(albumsResponse.data);
-            let responseData = photos.map( (photo) => {
-                const albumData = albums[photo.albumId];
-                const userData = users[albumData.userId];
-                return {
-                    id: photo.id,
-                    title: photo.title,
-                    url: photo.url,
-                    thumbnailUrl: photo.thumbnailUrl,
-                    album: {
-                        id: albumData.id,
-                        title: albumData.title,
-                        user: {
-                            ...userData
-                        }
-                    }
-                }
-            })
-            responseData = params && params?.title 
-                ? responseData.filter((photo) => photo.title.includes(params.title)) 
-                : responseData;
-            responseData = params && params['album.title']
-                ? responseData.filter((photo) => photo.album.title.includes(params['album.title'])) 
-                : responseData;
-            responseData = params && params['album.user.email']
-                ? responseData.filter((photo) => photo.album.user.email === params['album.user.email']) 
-                : responseData;
+            const enrichedData = getEnrichedData(photos, albums, users);
+            const filteredData = getFilteredData(enrichedData, params);
             const limit = params?.limit ? params.limit : 25;
             const offset = params?.offset ? params.offset : 0;
-            responseData = responseData.slice(offset, limit);
+            const responseData = filteredData.slice(offset, limit);
             response = { status: 200, data: responseData}
         }
     } catch (error) {
         console.error('Error', error)
     }
     return response;
+}
+
+const getEnrichedData = (photos, albums, users) => {
+    const enrichedData = photos.map( (photo) => {
+        const albumData = albums[photo.albumId];
+        const userData = users[albumData.userId];
+        return {
+            id: photo.id,
+            title: photo.title,
+            url: photo.url,
+            thumbnailUrl: photo.thumbnailUrl,
+            album: {
+                id: albumData.id,
+                title: albumData.title,
+                user: {
+                    ...userData
+                }
+            }
+        }
+    })
+    return enrichedData;
+}
+
+const getFilteredData = (data, params) => {
+    let filteredData = data;
+    if(params){
+        const filters = [];
+        if (params.title) {
+            filters.push((photo) => photo.title.includes(params.title));
+        }
+        if (params['album.title']) {
+            filters.push((photo) => photo.album.title.includes(params['album.title']));
+        }
+        if (params['album.user.email']) {
+            filters.push((photo) => photo.album.user.email === params['album.user.email']);
+        }
+        filteredData= data.filter((photo) => filters.every((filter) => filter(photo)));
+    }
+    return filteredData;
 }
 
 module.exports = {
